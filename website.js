3d5e08168a0b50bbcf1fc6bd2587a57870e33e4f
WeHaV <101073982+WeHaV@users.noreply.github.com>
2026-08-25 09:07:57 -0400

    Update website.js

--- website.js
+++ website.js
@@ -1,3 +1,4 @@
+let name = "";
 let count = 0;//this is a comment
 let count2 = 0;
 const e = 2.71828;
@@ -14,4 +15,13 @@ function increaseCount() {
 function decreaseCount() {
     count--;
     updateCount(count);
+}
+
+function updateDupName(name) {
+    document.getElementById("duplicated").innerHTML = name;
+}
+
+function changeName() {
+    name = document.getElementById("duplicated").value;
+    updateDupName(name + name);
 }
